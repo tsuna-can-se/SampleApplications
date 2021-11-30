47299417ef141c54e2f62b0bddd071335fe9b709
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2021-12-01 00:26:43 +0900

    ChangeTrackerの確認サンプルを追加

---
+++ ChangeTrackerConfirm/src/ChangeTrackerConfirm/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace ChangeTrackerConfirm.Models;
+
+public class Category
+{
+    public Category()
+    {
+        this.Name = string.Empty;
+        this.Products = new HashSet<Product>();
+    }
+
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Product> Products { get; set; }
+}
