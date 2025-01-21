831806e97abe87a580f81d484dd6cb33feb432b3
tsuna-can-se <61451753+tsuna-can-se@users.noreply.github.com>
2025-01-21 15:09:40 +0900

    `Product` クラスの `Id` 自動生成を無効化
    `Product` クラスの `Id` プロパティに付与されていた
    `[DatabaseGenerated(DatabaseGeneratedOption.Identity)]` 属性が削除されました。
    これにより、`Id` の値は手動で設定される必要があるか、
    別の方法で生成されることになります。

--- PkWithRecord/src/PkWithRecord/Data/Product.cs
+++ PkWithRecord/src/PkWithRecord/Data/Product.cs
@@ -4,7 +4,6 @@ namespace PkWithRecord.Data;
 
 public class Product
 {
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public ProductId Id { get; set; }
 
     public required string Name { get; set; }
